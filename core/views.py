0a36576840455a41b998c895a8aeee30b5e15ba9
Lucas Ismael Guerrero Rojas <lucas.guerrero04@inacapmail.cl>
2025-12-09 20:48:46 -0300

    arreglo completado

--- core/views.py
+++ core/views.py
@@ -8,31 +8,31 @@ from .serializers import (
     PlanMantencionSerializer,
     OrdenTrabajoSerializer,
 )
-
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 # Create your views here.
 
 class EmpresaViewSet(viewsets.ModelViewSet):
     queryset = Empresa.objects.all()
     serializer_class = EmpresaSerializer
-    
+    instruction_classes = [IsAuthenticatedOrReadOnly]
 
 class EquipoViewSet(viewsets.ModelViewSet):
     queryset = Equipo.objects.all()
     serializer_class = EquipoSerializer
-    
+    instruction_classes = [IsAuthenticatedOrReadOnly]
 
 class TecnicoViewSet(viewsets.ModelViewSet):
     queryset = Tecnico.objects.all()
     serializer_class = TecnicoSerializer
-    
+    instruction_classes = [IsAuthenticatedOrReadOnly]
 
 class PlanMantencionViewSet(viewsets.ModelViewSet):
     queryset = PlanMantencion.objects.all()
     serializer_class = PlanMantencionSerializer
-    
+    instruction_classes = [IsAuthenticatedOrReadOnly]
 
 class OrdenTrabajoViewSet(viewsets.ModelViewSet):
     queryset = OrdenTrabajo.objects.all()
     serializer_class = OrdenTrabajoSerializer
-    
+    instruction_classes = [IsAuthenticatedOrReadOnly]
